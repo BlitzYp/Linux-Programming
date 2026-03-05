e357983512b0cad1779a416476dd70c002e0b00a
BlitzYp <blitboi303@gmail.com>
2026-03-05 02:06:55 +0200

    Added flags to md_mem

--- md/mem/md_mem.c
+++ md/mem/md_mem.c
@@ -26,29 +26,35 @@ static void touch_pages(volatile unsigned char *p, size_t nbytes, size_t pagesz)
     }
 }
 
-static void *alloc_mmap(size_t n) 
+static void* alloc_mmap(size_t n) 
 {
-    void *p = mmap(NULL, n, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if (p == MAP_FAILED) return NULL;
+    void *p=mmap(NULL, n, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (p==MAP_FAILED) return NULL;
     return p;
 }
 
-static void *alloc_sbrk(size_t n) 
+static void* alloc_sbrk(size_t n) 
 {
-    void *old = sbrk(0);
-    void *res = sbrk((intptr_t)n);
-    if (res == (void*)-1) return NULL;
+    void *old=sbrk(0);
+    void *res=sbrk((intptr_t)n);
+    if (res==(void*)-1) return NULL;
     return old; // start of newly "allocated" region
 }
 
 int main(int argc, char **argv) 
 {
-    if (argc!=2) return -1;
+    // -t ir priekš touch (allokācija+rakstīt tajā lapā)
+    if ((argc!=2)||!(strcmp(argv[1],"malloc")==0 || strcmp(argv[1],"mmap")==0 || strcmp(argv[1],"sbrk")==0)) {
+        fprintf(stderr, "Usage: %s <malloc|mmap|sbrk> t\n", argv[0]);
+        return -1;
+    }
+    
     long ps=sysconf(_SC_PAGESIZE);
     size_t pagesz=(ps>0) ? (size_t)ps : 4096;
+    uint8_t method=0,touch=0;
+    if (argc==3&&strcmp(argv[2],"-t")==0) touch=1;
 
-    unsigned int method=0;
-    size_t count=0;
+    long long count=0;
     if (strcmp(argv[1],"malloc")==0) method=0;
     else if (strcmp(argv[1],"mmap")==0) method=1;
     else if (strcmp(argv[1],"sbrk")==0) method=2;
@@ -60,13 +66,13 @@ int main(int argc, char **argv)
         else if (method==2) p=alloc_sbrk(MB);
         if (!p) break;
         count++;
-        //touch_pages((volatile unsigned char*)p,MB,pagesz);
+        if (touch) touch_pages((volatile unsigned char*)p,MB,pagesz);
 
-        if (count%256==0) fprintf(stderr,"Allocted MB thus far: %d\n",count);
+        if (count%256==0) fprintf(stderr,"Allocted MB thus far: %lld\n",count);
     }
 
     printf("Results with method %s\n",argv[1]);
-    printf("Total memory allocated(bytes): %ld\n",count*MB);
-    printf("Total MB allocated: %d\n",count);
+    printf("Total memory allocated(bytes): %lld\n",count*MB);
+    printf("Total MB allocated: %lld\n",count);
     return 0;
 }
