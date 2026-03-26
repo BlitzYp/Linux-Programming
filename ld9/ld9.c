b82dd62617205e7fa7ec14e7353040ab72955933
BlitzYp <blitboi303@gmail.com>
2026-03-26 19:16:24 +0200

    Update

--- ld9/ld9.c
+++ ld9/ld9.c
@@ -3,10 +3,24 @@
 #include <unistd.h>
 #include <signal.h>
 
+static jmp_buf env;
+
+static void handle_signal(int s)
+{
+    if (s==SIGALRM) longjmp(env,1);
+}
+
 int main(void)
 {
 
-   // tālāko kodu nemainīt!
+    struct sigaction action={0};
+    action.sa_handler=handle_signal;
+    sigaction(SIGALRM,&action,NULL);
+
+    // Pirmajā reizē atgriež 0, katrā nākamā atgriež 1
+    if (setjmp(env)!=0) return 0;
+
+    // tālāko kodu nemainīt!
 
     // taimeris uz 3 sekundeem
     alarm(3);
@@ -16,5 +30,4 @@ int main(void)
 
     // atgriež kaut kādu nenulles kodu, lai kompilators nesūdzētos
     return -1;
-   return 0;
 }
